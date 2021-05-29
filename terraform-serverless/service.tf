cd4c8a97a265ee285a0a451533e1edab9af3f5dc
sydecaradmin <82789575+sydecaradmin@users.noreply.github.com>
2021-05-29 11:30:55 -0700

    Update service.tf

--- terraform-serverless/service.tf
+++ terraform-serverless/service.tf
@@ -50,6 +50,6 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
 
 # WORKAROUND 
 data "external" "image_digest" {
-  program = ["/bin/bash", "scripts/get_latest_tag.sh", var.project, local.service_name]
+  program = ["scripts/get_latest_tag.sh", var.project, local.service_name]
 }
 # END WORKAROUND
