d866189f75ee9ae0ec765275ed0920845856d3c8
sydecaradmin <82789575+sydecaradmin@users.noreply.github.com>
2021-05-28 22:02:01 -0700

    Update service.tf
    add path to bash

--- terraform-serverless/service.tf
+++ terraform-serverless/service.tf
@@ -50,6 +50,6 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
 
 # WORKAROUND 
 data "external" "image_digest" {
-  program = ["bash", "scripts/get_latest_tag.sh", var.project, local.service_name]
+  program = ["/bin/bash", "scripts/get_latest_tag.sh", var.project, local.service_name]
 }
 # END WORKAROUND
